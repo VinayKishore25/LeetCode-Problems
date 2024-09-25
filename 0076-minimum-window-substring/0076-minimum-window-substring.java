811320b4c3439767deae53067b743b07660e9f50
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2024-09-25 22:36:31 +0530

    Time: 19 ms (48.44%), Space: 45.4 MB (30.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,37 @@
 class Solution {
-    public String minWindow(String s, String t) {
-       int[] tcarr = new int[52];
-       for (char c: t.toCharArray()) {
-           tcarr[index(c)]++;
-       }
-       int[] scarr = new int[52];
-       String res = "";
-       int m = s.length();
-       int j = 0;
-       for (int i = 0; i < m; i++) {
-           char c = s.charAt(i);
-           scarr[index(c)]++;
-           while (j < m && allCharsIn(tcarr, scarr)) {
-               if (res.length() == 0 || res.length() > i + 1 - j) {
-                   res = s.substring(j, i+1);
-               }
-               scarr[index(s.charAt(j))]--;
-               j++;
-           }
-       }
-       return res;
-    }
-
-    private int index(char c) {
-        if (c - 'a' < 0) {
-            return c - 'A';
+    public String minWindow(String s, String p) {
+        HashMap<Character,Integer> hm2 = new HashMap<>();
+        HashMap<Character,Integer> hm1 = new HashMap<>();
+        for(char ch : p.toCharArray()){
+            hm2.put(ch,hm2.getOrDefault(ch,0)+1);
         }
-        return 'Z' - 'A' + 1 + c - 'a';
-    }
-
-    private boolean allCharsIn(int[] tcarr, int[] scarr) {
-        for (int i = 0; i < tcarr.length; i++) {
-            if (tcarr[i] > scarr[i]) {
-                return false;
+        int req = p.length();
+        int n = s.length();
+        int small = Integer.MAX_VALUE,up = Integer.MAX_VALUE;
+        int res = Integer.MAX_VALUE,j = 0;
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i);
+            if(hm2.containsKey(ch)){
+                if(hm1.getOrDefault(ch,0) < hm2.get(ch)){
+                    req--;
+                }
+            }
+            hm1.put(ch,hm1.getOrDefault(ch,0)+1);
+            while(req == 0){
+                char ch1 = s.charAt(j);
+                if((i - j < res)){
+                    small = j;
+                    up = i;
+                    res = i - j;
+                }
+                hm1.put(ch1,hm1.get(ch1) - 1);
+                if(hm2.containsKey(ch1) && hm1.get(ch1) < hm2.get(ch1)){
+                    req++;
+                }
+                j++;
             }
         }
-        return true;
+        if(small == Integer.MAX_VALUE || up == Integer.MAX_VALUE) return "";
+        return s.substring(small,up+1);
     }
 }

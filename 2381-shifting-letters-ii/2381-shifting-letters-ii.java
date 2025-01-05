bc37aa2fd87b32911626c729b16a5a716505345f
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-01-05 22:20:24 +0530

    Time: 2453 ms (5.19%), Space: 67.3 MB (100%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void makeChanges(int start, int end, int val,int[] prefix){
+        for(int i = start; i <= end ; i++){
+            prefix[i] += val;
+        }
+    }
+    
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length();
+        int l = shifts.length;
+        int[] prefix = new int[n];
+        HashMap<Integer,Character> hm = new HashMap<>();
+        for(int i = 0 ; i < 26 ; i++){
+            hm.put(i,(char)(i+'a'));
+        } 
+        for(int i = 0 ; i < l ; i++){
+            int val = 1;
+            if(shifts[i][2] == 0){
+                val = -1;
+            }
+            makeChanges(shifts[i][0],shifts[i][1],val,prefix);
+        }
+        String res = "";
+        for(int i = 0 ; i < n ; i++ ){
+            char ch = s.charAt(i);
+            res += hm.get((ch - 'a' + prefix[i] + (1300000))%26);
+        }
+        return res;
+    }
+}

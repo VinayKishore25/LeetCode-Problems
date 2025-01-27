863f0fb7ed069b38a13ebd147925c075567b45e0
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-01-27 22:19:18 +0530

    Time: 6 ms (74.42%), Space: 45.9 MB (23.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
         int n = prerequisites.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        boolean[] visited = new boolean[numCourses];
         int[] indegree = new int[numCourses];
         for(int i = 0 ; i < numCourses ; i++){
             adj.add(new ArrayList<>());
         }
         for(int i = 0 ; i < n ; i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            // adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
             indegree[prerequisites[i][0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        boolean[] vis = new boolean[numCourses];
-        int count = 0;
-        for(int i = 0 ; i < numCourses ; i++){
-            if(indegree[i] == 0) {
+        for(int i = 0; i < numCourses ; i++){
+            if(indegree[i] == 0){
                 q.add(i);
-                vis[i] = true;
+                // numCourses--;
             }
         }
         while(!q.isEmpty()){
-            int x = q.poll();
-            // System.out.println(x + " array " + adj.get(x));
-            count++;
-            for(int each : adj.get(x)){
-                indegree[each]--;
-                if(indegree[each] == 0){
-                    q.add(each);
+            int course = q.poll();
+            numCourses--;
+            for(int neighbour : adj.get(course)){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0){
+                    q.add(neighbour);
                 }
             }
         }
-        return count == numCourses;
+        return numCourses == 0;
     }
 }

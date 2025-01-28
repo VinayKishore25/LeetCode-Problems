3c687643722737bb84ee243d4db3e7a372bb3e33
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-01-28 10:14:29 +0530

    Time: 6 ms (34.17%), Space: 44.8 MB (86.55%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -1,26 +1,27 @@
 class Solution {
-    int maxFish = 0;
-    public void dfs(int[][] grid,boolean[][] vis,int i,int j, int n,int m,int[] currentFish){
+    
+    public int dfs(int[][] grid,boolean[][] vis,int i,int j,int n,int m){
         if(i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || grid[i][j] == 0){
-            return;
+            return 0;
         }
         vis[i][j] = true;
-        currentFish[0]+=grid[i][j];
-        maxFish = Math.max(maxFish,currentFish[0]);
+        int fish = grid[i][j];
         int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
         for(int k = 0 ; k < 4 ; k++){
-            dfs(grid,vis,i+directions[k][0],j+directions[k][1],n,m,currentFish);
+            fish += dfs(grid,vis,i+directions[k][0],j+directions[k][1],n,m);
         }
-
+        return fish;
     }
     public int findMaxFish(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int maxFish = 0;
         boolean[][] vis = new boolean[n][m];
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(!vis[i][j] && grid[i][j] > 0){
-                    dfs(grid,vis,i,j,n,m,new int[1]);
+                    int currentFish = dfs(grid,vis,i,j,n,m);
+                    maxFish = Math.max(maxFish,currentFish);
                 }
             }
         }

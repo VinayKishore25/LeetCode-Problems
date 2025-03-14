92fcb68578c4e6fbf48e5fd7c0d6ae9cc59e1961
VinayKishore25 <141477458+VinayKishore25@users.noreply.github.com>
2025-03-14 23:10:56 +0530

    Time: 2 ms (74.61%), Space: 57.4 MB (98.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,11 +4,10 @@ class Solution {
         int[] dp = new int[n];
         dp[0] = 0;
         int max = 0;
+        int min = 10000000;
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j <= i ; j++){
-                dp[i] = Math.max(dp[i],prices[i] - prices[j]);
-            }
-            max = Math.max(max,dp[i]);
+            max = Math.max(max,prices[i] - min);
+            min = Math.min(min,prices[i]);
         }
         return max;
     }

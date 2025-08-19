0a51a6751b5bda8f430bd63e2f9124e5f4fc8cc0
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2025-08-19 11:46:32 +0530

    Time: 5 ms (60.24%), Space: 44.9 MB (88.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,14 +4,14 @@ class Solution {
         int m = word2.length();
         int[][] dp = new int[n+1][m+1];
         for(int i = 0 ; i <= n ; i++){
-            dp[i][0] = i;
-        }
-        for(int i = 0 ; i <= m ; i++){
-            dp[0][i] = i;
-        }
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
+            for(int j = 0 ; j <= m ; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+                else if(j == 0){
+                    dp[i][j] = i;
+                }
+                else if(word1.charAt(i-1) == word2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else{

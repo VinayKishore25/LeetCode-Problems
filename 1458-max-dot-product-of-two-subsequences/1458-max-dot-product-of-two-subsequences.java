b543eae46bde0f01e109d19d3ff558d4d6f45c0d
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-01-08 23:11:21 +0530

    Time: 12 ms (45.19%), Space: 46.7 MB (31.85%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -14,13 +14,27 @@ class Solution {
         // }
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                dp[i][j] = Math.max(dp[i-1][j-1],
-                                        Math.max(dp[i-1][j],
-                                                    Math.max(dp[i][j-1],
-                                                            Math.max(dp[i-1][j-1] + nums1[i-1] * nums2[j-1],nums1[i-1]*nums2[j-1])
-                                                            )
-                                                )
-                                    );
+                int max = nums1[i-1] * nums2[j-1];
+                if(dp[i-1][j] > max){
+                    max = dp[i-1][j];
+                }
+                if(dp[i-1][j-1] > max){
+                    max = dp[i-1][j-1];
+                }
+                if(dp[i][j-1] > max){
+                    max = dp[i][j-1];
+                }
+                if(dp[i-1][j-1] + nums1[i-1] * nums2[j-1] > max){
+                    max = dp[i-1][j-1] + nums1[i-1] * nums2[j-1];
+                }
+                dp[i][j] = max;
+                // dp[i][j] = Math.max(dp[i-1][j-1],
+                //                         Math.max(dp[i-1][j],
+                //                                     Math.max(dp[i][j-1],
+                //                                             Math.max(dp[i-1][j-1] + nums1[i-1] * nums2[j-1],nums1[i-1]*nums2[j-1])
+                //                                             )
+                //                                 )
+                //                     );
             }
         }
         return dp[n][m];

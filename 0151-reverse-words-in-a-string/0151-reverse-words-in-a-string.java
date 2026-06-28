5866db55e10de85f862268bdb7565d5e3006deb4
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-06-28 18:24:30 +0530

    Time: 13 ms (5.82%), Space: 47.5 MB (5.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,23 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
         int n = s.length();
-        int end = n - 1, start = n - 1;
+        int end = n, start = n - 1;
         String res = "";
         String word = "";
+        boolean flag = false;
         while(end >= 0){
-            while(end >= 0 && s.charAt(end) == ' '){
+            while(end > 0 && s.charAt(end - 1) == ' '){
                 end--;
             }
+            if(end == 0) break;
             start = end;
-            while(start >=0 && s.charAt(start) != ' '){
-                word = s.charAt(start) + word;
+            while(start > 0 && s.charAt(start - 1) != ' '){
+                word = s.charAt(start - 1) + word;
                 start--;
             }
-            word += " ";
+            if(flag){
+                res += " ";
+            }
+            flag = true;
             res += word;
             word = "";
             end = start;
         }
-        return res.trim();
+        return res;
     }
 }

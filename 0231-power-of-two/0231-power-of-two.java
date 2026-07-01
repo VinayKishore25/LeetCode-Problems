7383d5425e646855c421f0c1ac034b4ff983d3d7
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-07-01 21:15:42 +0530

    Time: 1 ms (96.52%), Space: 42.4 MB (72.49%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0) return false;
-        if(n<1)return false;
-        return (n&(n-1))==0;
+        if(n < 1) return false;
+        return (n & n - 1) == 0;
     }
 }

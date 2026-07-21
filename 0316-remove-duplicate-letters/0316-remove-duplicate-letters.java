27103f2f6c24ad0b691ef0cf68cde8e1d85c2c2e
MUNJULURI V V D SURYA KISHORE VINAY <vinaykishore2512@gmail.com>
2026-07-22 00:13:37 +0530

    Time: 2 ms (85.71%), Space: 44 MB (34.66%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        boolean[] visited = new boolean[26];
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']--;
+            if(visited[ch - 'a']){
+                continue;
+            }
+            while(!st.isEmpty() && st.peek() > ch && freq[st.peek() - 'a'] > 0 ){
+                visited[st.peek() - 'a'] = false;
+                st.pop();
+            }
+            st.push(ch);
+            visited[ch - 'a'] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
